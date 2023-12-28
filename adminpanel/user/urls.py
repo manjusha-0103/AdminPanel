5aa6c6019dc48ba6a5d7cb01af21c1b6f58d6436
Manjusha <93437320+manjusha-0103@users.noreply.github.com>
2023-12-28 23:24:40 +0530

    Delete adminpanel directory

--- adminpanel/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_view, dashboard, logout_view
-
-urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('dashboard/', dashboard, name='dashboard'),
-    path('logout/', logout_view, name='logout'),
-]
